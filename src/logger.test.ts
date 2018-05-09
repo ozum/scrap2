143fe779e972b821512bab5a03cc0aa123fe4b96
Ozum <ozum@ozum.net>
2018-05-09 15:12:46 +0300

    fix: some changes

--- src/logger.test.ts
+++ src/logger.test.ts
@@ -3,5 +3,6 @@ import { stubLogger } from "./logger";
 describe("stubLogger", () => {
   it("should conform interface", () => {
     stubLogger.silly("msg");
+    // expect(true).toBe(false);
   });
 });
